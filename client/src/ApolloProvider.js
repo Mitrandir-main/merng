680d409f1be63e19c9eb91378093d7250191be0c
unknown <you@example.com>
2020-10-28 19:29:39 +0200

    backend v2 with finished v1 of frontend

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,28 +1,29 @@
+import React from "react";
+import App from "./App";
+import { setContext } from "apollo-link-context";
+
 import {
   ApolloClient,
   ApolloProvider,
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
-import React from "react";
-import App from "./App";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:5000",
 });
 
-// const authLink = setContext(() => {
-//   const token = localStorage.getItem("jwtToken");
-//   return {
-//     headers: {
-//       Authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  //   link: authLink.concat(httpLink),
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/client";
-// import gql from "apollo-server";
-import gql from "graphql-tag";
 import { Grid, Transition } from "semantic-ui-react";
+
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
-// const { gql } = require("apollo-server");
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 function Home() {
-  const {
-    loading,
-    data: { getPosts: posts },
-  } = useQuery(FETCH_POSTS_QUERY);
+  const { user } = useContext(AuthContext);
+  const { loading, data: { getPosts: posts } = {} } = useQuery(
+    FETCH_POSTS_QUERY
+  );
 
   return (
     <Grid columns={3}>
       <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
-
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
-          <h1>Loading posts...</h1>
+          <h1>Loading posts..</h1>
         ) : (
           <Transition.Group>
             {posts &&
@@ -35,26 +40,5 @@ function Home() {
     </Grid>
   );
 }
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
