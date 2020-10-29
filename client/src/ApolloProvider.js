7264baca0e4218c51c71e8d21899e42a89d35295
Mitrandir-main <dian.kumanov@abv.bg>
2020-10-29 21:49:57 +0200

    Functional build with deployment

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://powerful-sea-17409.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
